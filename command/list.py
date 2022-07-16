8316e07a4ad92450792232933c1c7a03e9744b83
caleberi <caleberioluwa@gmail.com>
2022-07-16 08:03:09 +0100

    fix : add  list command fix

--- command/list.py
+++ command/list.py
@@ -1,7 +1,5 @@
-from os import getcwd, listdir, path
-
 import click
-
+from os import getcwd, listdir, path
 from command.utils.helpers import resolve_os_encoding
 
 
@@ -14,11 +12,13 @@ from command.utils.helpers import resolve_os_encoding
 )
 def list_files_destination(folder_name):
     """
+    List out all filepath that is in the folder
+    
     :params `folder_name` - path to list out
     """
     destination_folder_path = path.join(resolve_os_encoding(getcwd()), folder_name)
     files = listdir(destination_folder_path)
-    print(f"::: FILES :${destination_folder_path}:::")
+    click.echo(f"::: FILES :${destination_folder_path}:::")
     for file in files:
-        print(f"\t> {file}")
-    print("> DONE .")
+        click.echo(f"\t> {file}")
+    click.echo("> DONE .")
