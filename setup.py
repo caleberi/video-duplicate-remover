b36bc58eaa7588caef79ef96e49ab12c7b0dc1ca
caleberi <caleberioluwa@gmail.com>
2022-07-16 08:01:02 +0100

    doc : fix setup

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import find_packages, setup
 
-with open("README.md", 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
     name="video-cleaner",
     version="0.1.0",
-    description='A video cleaning cli application',
+    description="A video cleaning cli application",
     license="MIT",
     long_description=long_description,
-    author='caleberi',
-    author_email='caleberioluwa@gmail.com',
+    author="caleb eri",
+    author_email="caleberioluwa@gmail.com",
+    url="https://github.com/caleberi/video-cleaner",
     packages=find_packages(),
     include_package_data=True,
-    requires=[
+    install_requires=[
         "click",
         "black",
     ],
-    entry_points={"console_scripts": ["main = main:cli"]},
+    entry_points={"console_scripts": ["video-cleaner-cli = command.main:cli"]},
 )
